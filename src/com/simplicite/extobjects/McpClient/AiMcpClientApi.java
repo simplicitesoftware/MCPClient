9a49cd8bd2a6c22fbf5bc2f136fef13dc16f79ba
designer (Designer) <>
2026-09-01 16:16:33 +0200

    add simpleChatBot endpoint

--- src/com/simplicite/extobjects/McpClient/AiMcpClientApi.java
+++ src/com/simplicite/extobjects/McpClient/AiMcpClientApi.java
@@ -3,13 +3,15 @@ package com.simplicite.extobjects.McpClient;
 
 import org.json.JSONObject;
 import org.json.JSONArray;
-
+import  java.util.List;
 import io.modelcontextprotocol.spec.McpSchema.CallToolResult;
 
 import com.simplicite.util.Grant;
 import com.simplicite.util.AppLog;
 import com.simplicite.util.Tool;
 import com.simplicite.util.Message;
+import com.simplicite.util.tools.HTMLTool;
+import com.simplicite.util.tools.MarkdownTool;
 import com.simplicite.util.exceptions.HTTPException;
 import com.simplicite.util.tools.Parameters;
 import com.simplicite.commons.McpClient.LlmTools;
@@ -28,9 +30,11 @@ public class AiMcpClientApi extends com.simplicite.webapp.services.RESTServiceEx
     private static final String JSON_REQ_TYPE = "reqType";
     public static  boolean mcpMuteTools;
 
+
     @Override
     public void init(Parameters params) {
         Grant g = getGrant();
+
         if (LlmTools.AI_DEBUG_LOGS) AppLog.info("init API with GRANT " + g.getLogin());
         manager = McpClientManager.getInstance(g);
         tools = manager.listToolsAsOpenAIFormat();
@@ -62,16 +66,23 @@ public class AiMcpClientApi extends com.simplicite.webapp.services.RESTServiceEx
             AppLog.info("McpClientApi POST tools: " + tools.toString(1), getGrant());
         }
         try {
+            List<String> parts = params.getLocationParts();
             JSONObject req = params.getJSONObject();
             String prompt = getParamOrreqParam(PARAMS_PROMPT_KEY, params, req);
-            String type = getParamOrreqParam(JSON_REQ_TYPE, params, req);
-            if (Tool.isEmpty(type)) type = "default";
+            String type = parts.size() > 3? parts.get(3): getParamOrreqParam(JSON_REQ_TYPE, params, req);
+            if (Tool.isEmpty(type))
+                type = "default";
 
             switch (type) { // use switch for future extension
                 case "provider":
                     return new JSONObject().put("provider", LlmTools.provider());
                 case "chatBot":
                     return chatbotCaller(prompt, params, req);
+                case "simpleChatBot":
+                    String id = req.has("chat_id")?req.getString("chat_id"):getGrant().getSessionId();
+                    String msg = req.optString("message");
+                     return new JSONObject().put("message", msg).put("reply", simpleChatbotCaller(msg,id));
+                    //return ;
                 case "BOT_NAME":
                     return new JSONObject().put("botName", LlmTools.getBotName());
                 case "CHECK_SPEECH_RECOGNITION":
@@ -93,6 +104,137 @@ public class AiMcpClientApi extends com.simplicite.webapp.services.RESTServiceEx
             return error(e);
         }
     }
+    private String simpleChatbotCaller(String prompt, String id) {
+        if(Tool.isEmpty(prompt)) return "You said nothing :-(";
+        JSONObject result = simpleChatbotCaller(prompt,id,null,null);
+        JSONObject choice = result.optJSONObject("response", new JSONObject()).optJSONArray("choices", new JSONArray()).optJSONObject(0, new JSONObject());
+
+        while("tool_calls".equals(choice.optString("finish_reason"))){
+
+            JSONArray toolCalls = choice.optJSONObject("message",new JSONObject()).optJSONArray("tool_calls", new JSONArray());
+            result = simpleChatbotCaller(prompt,id, toolCalls, new JSONArray());
+            choice = result.optJSONObject("response", new JSONObject()).optJSONArray("choices", new JSONArray()).optJSONObject(0, new JSONObject());
+        }
+        String res= choice.optJSONObject("message", new JSONObject()).optString("content", "not found");
+        return   HTMLTool.toSafeHTML(MarkdownTool.toHTML(res));
+
+
+    }
+    private JSONObject simpleChatbotCaller(
+        String prompt,
+        String id,
+        JSONArray acceptedTools,
+        JSONArray refusedTools) {
+    boolean istool = false;
+    try {
+        if (Tool.isEmpty(prompt)) {
+            return badRequest("need a prompt");
+        }
+        JSONArray promptJson = new JSONArray().put(new JSONObject().put("type", "text").put("text", prompt.trim()));
+        prompt = promptJson.toString();
+        if (LlmTools.AI_DEBUG_LOGS) {
+            AppLog.info("McpClientApi POST request", getGrant());
+        }
+
+        JSONArray historic = getHistoric(id);
+        if (tools.length() == 0) {
+            AppLog.warning("NO MCP TOOLS FOUND, Call api without tools");
+            JSONObject result = LlmTools.aiCaller(getGrant(), serverInstructions, historic, prompt);
+            addHist(id,result,prompt,null,null);
+            return result;
+        } else {
+
+            JSONArray assistantToolsCalls = new JSONArray();
+            JSONArray userToolsResponse = new JSONArray();
+            JSONArray p = LlmTools.optJSONArray(prompt);
+            if (acceptedTools != null && acceptedTools.length() > 0
+                    || refusedTools != null && refusedTools.length() > 0) {
+
+                for (int i = 0; i < acceptedTools.length(); i++) {
+                    JSONObject tool = acceptedTools.getJSONObject(i);
+                    if (tool.has("description")) {
+                        tool.remove("description");
+                    }
+                    if (tool.has("index")) {
+                        tool.remove("index");
+                    }
+                    tool.remove("index");
+                    assistantToolsCalls.put(tool);
+                    JSONObject userToolResponse = new JSONObject();
+                    userToolResponse.put("tool_call_id", tool.optString("id"));
+                    userToolResponse.put("role", "tool");
+                    userToolResponse.put(LlmTools.CONTENT_KEY, getToolResponse(tool));
+                    userToolsResponse.put(userToolResponse);
+                }
+                for (int i = 0; i < refusedTools.length(); i++) {
+
+                    JSONObject tool = refusedTools.getJSONObject(i);
+                    if (tool.has("description")) {
+                        tool.remove("description");
+                    }
+                    if (tool.has("index")) {
+                        tool.remove("index");
+                    }
+                    tool.remove("index");
+                    assistantToolsCalls.put(tool);
+                    JSONObject userToolResponse = new JSONObject();
+                    userToolResponse.put("tool_call_id", tool.optString("id"));
+                    userToolResponse.put("role", "tool");
+                    userToolResponse.put(LlmTools.CONTENT_KEY, "Tool execution denied by user. Ask the user what they would like to do now");
+                    userToolsResponse.put(userToolResponse);
+                }
+            }
+            if(istool ){
+                int index = historic.length() -1;
+                if(index >= 0 && "assistant".equals(historic.getJSONObject(index).getString("role"))) historic.remove(index);
+
+            }
+            JSONObject response =
+                    LlmTools.aiCallerWithMCP(
+                            getGrant(),
+                            serverInstructions,
+                            historic,
+                            Tool.isEmpty(p) ? prompt : p,
+                            tools,
+                            assistantToolsCalls,
+                            userToolsResponse);
+            if (LlmTools.AI_DEBUG_LOGS) {
+                AppLog.info("McpClientApi POST response: " + response.toString(1), getGrant());
+            }
+            JSONArray toolCall = new JSONArray();
+            if ("tool_calls"
+                    .equals(
+                            response.optJSONArray("choices", new JSONArray())
+                                    .optJSONObject(0, new JSONObject())
+                                    .optString("finish_reason", ""))) {
+
+                toolCall =
+                        response.getJSONArray("choices")
+                                .getJSONObject(0)
+                                .optJSONObject("message", new JSONObject())
+                                .optJSONArray("tool_calls", new JSONArray());
+                for (int i = 0; i < toolCall.length(); i++) {
+                    JSONObject tool = toolCall.getJSONObject(i);
+                    String toolName =
+                            tool.optJSONObject("function", new JSONObject())
+                                    .optString("name", "");
+                    String toolDescription = manager.getToolDescription(toolName);
+                    tool.put("description", toolDescription);
+                }
+            }
+
+            JSONObject result = new JSONObject()
+                    .put("tools", toolCall)
+                    .put("request", prompt)
+                    .put("response", response);
+            addHist(id,result,Tool.isEmpty(p) ? prompt : p,userToolsResponse,assistantToolsCalls);
+            return result;
+        }
+    } catch (Exception e) {
+        AppLog.error("McpClientApi POST error: " + e.getMessage(), e, getGrant());
+        return error(e);
+    }
+}
 
     private Object recallWithTools(String prompt, Parameters params, JSONObject req) {
         try {
